ed2681d82d55eabc09c026e6da755af5f2c72dc3
youssefimajat <95992103+youssefimajat@users.noreply.github.com>
2022-04-06 18:15:51 -0500

    LAB9

---
+++ LAB9/main.js
@@ -0,0 +1,17 @@
+const student1 = new Student(10);
+student1.addAnswer(new Question(2, 'a'));
+student1.addAnswer(new Question(3, 'b'));
+student1.addAnswer(new Question(1, 'b'));
+const student2 = new Student(11);
+student2.addAnswer(new Question(3, 'b'));
+student2.addAnswer(new Question(2, 'a'));
+student2.addAnswer(new Question(1, 'd'));
+const students = [student1, student2];
+const questions = [new Question(1, 'b'), new Question(2, 'a'), new Question(3, 'b')];
+const quiz = new Quiz(questions, students);
+let scoreforStudent10 = quiz.scoreStudentBySid(10);
+console.log(scoreforStudent10); //Expected Result: 3
+let scoreforStudent11 = quiz.scoreStudentBySid(11);
+console.log(scoreforStudent11); //Expected Result: 2
+let average = quiz.getAverageScore();
+console.log(average); //Expected Reuslt: 2.5
